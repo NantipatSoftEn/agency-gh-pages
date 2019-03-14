b655484a18b18983578d9d151f1bef6eb792eee6
Army <keymasterviriya1150@gmail.com>
2019-03-14 21:35:01 +0700

    :art: :fire:

--- resources/views/Article/form-article.blade.php
+++ resources/views/Article/form-article.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.index')
+@extends('layouts/index')
 @section('content')
 <section id="services">
     <div class="container">
@@ -29,25 +29,7 @@
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
-                <!-- with update -->
-                  <form class="" action="{{url('/article-upd')}}" method="post" enctype="multipart/form-data">
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
-                    <input type="hidden" name="article_id" value="3">
-                    <input type="hidden" name="path_pic" value="C:\Users\stura\Desktop\agency-gh-pages\public\article\1552494022.jpg">
-                    <div class="form-group">
-                        <label for="exampleFormControlFile1">Example file input</label>
-                        <input type="file" class="form-control-file" name="picture" id="exampleFormControlFile1">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Title</label>
-                        <input type="text" class="form-control" name="title" placeholder="">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" name="content" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+            
                 <!-- with delete -->
                  <form action="{{url('/article-del')}}"  method="POST"  enctype="multipart/form-data">
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
