63f810cd302e7687b195185f04c83d5a0fa402ea
Army <keymasterviriya1150@gmail.com>
2019-03-14 20:46:28 +0700

    change name folder

--- resources/views/Article/show-article.blade.php
+++ resources/views/Article/show-article.blade.php
@@ -10,24 +10,25 @@
         </div> --}}
         <h2 class="section-heading text-uppercase">Show article</h2>
         <div class="row">
-
-            @for ($i = 0; $i < 5; $i++) <div class="col-md-4">
+            @foreach ($article as $item)
+            <div class="col-md-4">
                 <div class="card" style=" margin-bottom: 25px;">
-                    <img class="card-img-top" src="{{asset('/img/about/1.jpg')}}" alt="Card image cap">
+                    <img class="card-img-top" src="{{asset($item->path_pic)}}" alt="">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="{{asset('/edit-article')}}" class="btn btn-info  btn-sm">Edit</a>
+                        <h5 class="card-title">{{$item->title}}</h5>
+                    <p class="card-text">{{$item->content}}</p>
+                        <a href="{{asset('/edit-article/'.$item->id)}}" class="btn btn-info  btn-sm">Edit</a>
                         <button type="button" class="btn btn-danger  btn-sm">delect</button>
 
                         {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
                     </div>
                 </div>
-        </div>
-        @endfor
+            </div>
+            @endforeach
 
-    </div>
+
+
+        </div>
     </div>
 </section>
 @endsection
