831a82246d0fb47caf34cfd9884c82a6df7df563
Army <keymasterviriya1150@gmail.com>
2019-03-14 21:34:36 +0700

    :sparkles: edit page

--- resources/views/Article/edit-article.blade.php
+++ resources/views/Article/edit-article.blade.php
@@ -10,26 +10,28 @@
         </div> --}}
         <div class="row">
             <div class="col-md-12">
-                <h2 class="section-heading text-uppercase">Edit article</h2>
-                <form class="" action="" method="post" enctype="multipart/form-data">
+                <h2 class="section-heading text-uppercase">Form article</h2>
+                <img src="{{$article->path_pic}}" class="img-fluid">
+                <form class="" action="{{url('/article-upd')}}" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <input type="hidden" name="article_id" value="{{$article->id}}">
+                    <input type="hidden" name="path_pic" value="{{$article->path_pic}}">
                     <div class="form-group">
                         <label for="exampleFormControlFile1">Example file input</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                        <input type="file" class="form-control-file" name="picture" id="exampleFormControlFile1">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Title</label>
-                        <input type="text" class="form-control" name="title" placeholder="">
+                        <input type="text" class="form-control" name="title" placeholder="" value="{{$article->title}}">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">URL</label>
-                        <input type="text" class="form-control" name="url" placeholder="">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Content</label>
-                        <textarea class="form-control"  name="content" rows="3"></textarea>
+                        <label for="">Example textarea</label>
+                        <textarea class="form-control" id="" name="content" rows="3">{{$article->content}}"</textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
+
+
             </div>
         </div>
     </div>
