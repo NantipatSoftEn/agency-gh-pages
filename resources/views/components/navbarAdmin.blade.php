9d57d2815c35432ee9d630298700709991fca3a3
Army <keymasterviriya1150@gmail.com>
2019-03-16 17:32:22 +0700

    AUTH :sparkles:

--- resources/views/components/navbarAdmin.blade.php
+++ resources/views/components/navbarAdmin.blade.php
@@ -3,6 +3,9 @@
         {{-- <a class="navbar-brand" href="#">Navbar</a> --}}
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav text-uppercase ml-auto">
+                <li class="nav-item">
+                        <a class="nav-link" href="{{url('/')}}">main page</a>
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
@@ -13,7 +16,6 @@
                         <a class="dropdown-item" href="{{url('/show-album')}}">See Gallory</a>
                     </div>
                 </li>
-
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
@@ -24,8 +26,29 @@
                         <a class="dropdown-item" href="{{url('/show-article')}}">See Article</a>
                     </div>
                 </li>
+
+
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" v-pre style="background-color: #ffb3b3;">
+                        username {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
             </ul>
-         
+
+
+
         </div>
     </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,43 @@
 <?php
 
-/* 
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
-// Route::get('/', function () { 
-//     return view('welcome');
-// });
+
+
+        //////////////--GET--//////////////
+
+        //////////////Gallery//////////////
+        Route::get('/form-album', 'GalleryController@create');
+        Route::get('/edit-album/{id}', 'GalleryController@edit');
+        Route::get('/show-album', 'GalleryController@index');
+        Route::get('/edit-album-detail/{id}','GalleryController@edit_album_detail');
+        //////////////Article//////////////
+        Route::get('/edit-article/{id}', 'ArticleController@edit');
+        Route::get('/show-article', 'ArticleController@index');
+        Route::get('/form-article', 'ArticleController@create');
+        /////<---------------------->//////
+
+        //////////////--POST--/////////////
+
+        //////////////Gallery//////////////
+        Route::post('/form-album', 'GalleryController@upload');
+        Route::post('/album-ins', 'GalleryController@insert');
+        Route::post('/album-upd/{id}', 'GalleryController@update');
+        //////////////Article//////////////
+        Route::post('/article-ins', 'ArticleController@insert');
+        Route::post('/article-upd', 'ArticleController@update');
+        //////////////Picture//////////////
+        Route::post('/pic-upload', 'PictureController@upload');
+        /////<---------------------->//////
+
+        //////////////--DELETE--////////////
+        Route::delete('/album-del/{id}', 'GalleryController@delete');
+        Route::delete('/article-del/{id}', 'ArticleController@delete');
+        Route::delete('/pic-del/{id}', 'PictureController@delete');
+        /////<---------------------->//////
 
 
 Route::get('/', function () {
     return view('index');
 });
-//////////////--GET--//////////////
-
-//////////////Gallery//////////////
-Route::get('/form-album', 'GalleryController@create');
-Route::get('/edit-album/{id}', 'GalleryController@edit');
-Route::get('/show-album', 'GalleryController@index');
-Route::get('/edit-album-detail/{id}','GalleryController@edit_album_detail');
-//////////////Article//////////////
-Route::get('/edit-article/{id}', 'ArticleController@edit');
-Route::get('/show-article', 'ArticleController@index');
-Route::get('/form-article', 'ArticleController@create');
-/////<---------------------->//////
-
-//////////////--POST--/////////////
-
-//////////////Gallery//////////////
-Route::post('/form-album', 'GalleryController@upload');
-Route::post('/album-ins', 'GalleryController@insert');
-Route::post('/album-upd/{id}', 'GalleryController@update');
-//////////////Article//////////////
-Route::post('/article-ins', 'ArticleController@insert');
-Route::post('/article-upd', 'ArticleController@update');
-//////////////Picture//////////////
-Route::post('/pic-upload', 'PictureController@upload');
-/////<---------------------->//////
-
-//////////////--DELETE--////////////
-Route::delete('/album-del/{id}', 'GalleryController@delete');
-Route::delete('/article-del/{id}', 'ArticleController@delete');
-Route::delete('/pic-del/{id}', 'PictureController@delete');
-/////<---------------------->//////
