94cb4dccbdbb07d0ca8be3f8d905c20ab23ee5fa
Army <keymasterviriya1150@gmail.com>
2019-03-16 11:39:58 +0700

    add dropdown navbar  :bookmark:  :green_apple:

--- resources/views/components/1navbar.blade.php
+++ resources/views/components/1navbar.blade.php
@@ -8,7 +8,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav text-uppercase ml-auto">
-                <li class="nav-item">
+                {{-- <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="#services">Product</a>
                 </li>
                 <li class="nav-item">
@@ -22,6 +22,27 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                </li> --}}
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Gallory
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{url('/form-album')}}">Add Gallory</a>
+                        <a class="dropdown-item" href="{{url('/show-album')}}">See Gallory</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Article
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{url('/form-article')}}">Add Article</a>
+                        <a class="dropdown-item" href="{{url('/show-article')}}">See Article</a>
+                    </div>
                 </li>
             </ul>
         </div>
