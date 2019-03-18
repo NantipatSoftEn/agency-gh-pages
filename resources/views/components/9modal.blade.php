9b164cb645b98b6a8825d814725dc8f75525839a
Army <keymasterviriya1150@gmail.com>
2019-03-18 20:09:10 +0700

    children_crossing: Improving user experience / usability.

--- resources/views/components/9modal.blade.php
+++ resources/views/components/9modal.blade.php
@@ -1,15 +1,20 @@
 @foreach ($gallery as $item)
-<div class="modal fade" id="gallery-{{$item->id}}" role="dialog">
-    <div class="modal-dialog">
+<div class="modal fade bd-example-modal-lg" id="gallery-{{$item->id}}" role="dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">Modal Header</h4>
             </div>
+            <center><iframe  width="560" height="500"  src="{{$item->link}}"></iframe></center>
             <div class="modal-body">
-                <center><iframe src="{{$item->link}}"></iframe></center>
-                @foreach ($picture[$item->id] as $pic)
-            <img src="{{asset($pic->path)}}" class="img-thumbnail">
-                @endforeach
+                <div class="row">
+                    @foreach ($picture[$item->id] as $pic)
+                    <div class="col-md-4">
+                        <img src="{{asset($pic->path)}}" class="card-img-top">
+                    </div>
+
+                    @endforeach
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
