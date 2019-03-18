efef153a047f07efb1948d22faa5a3e5ba04c933
Army <keymasterviriya1150@gmail.com>
2019-03-18 21:23:42 +0700

    :label: Adding or updating types   :ambulance:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Carbon\Carbon;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call([articleSeeder::class,gallerySeeder::class,pictureSeeder::class,userSeeder::class
+        ]);
+     
     }
 }
